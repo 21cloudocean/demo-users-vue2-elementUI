d91ef09f727529fe9f7a55cd45cb99ee6fafb7b2
21cloudocean <437508630@qq.com>
2022-08-08 22:26:37 +0800

    使用elementUI中的table

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h1>UserList</h1>
+    <!-- 用户的表格 -->
+    <el-table :data="userList" stripe border>
+      <!-- 每列信息 -->
+      <!-- 第一列是索引列，只要type写成index，它就是索引列-->
+      <el-table-column type="index" label="#"> </el-table-column>
+      <el-table-column label="姓名" prop="name"> </el-table-column>
+      <el-table-column label="年龄" prop="age"> </el-table-column>
+      <el-table-column label="头衔" prop="position"> </el-table-column>
+      <el-table-column label="创建时间" prop="addtime"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -20,9 +29,10 @@ export default {
   methods: {
     async getUserList() {
       const { data: res } = await this.$http.get('/api/users')
-      // console.log(res)
+
       // res.status 等于 0 表示数据请求成功，否则，请求失败
       if (res.status !== 0) return console.log('用户列表数据请求失败！')
+      console.log(res.data)
       this.userList = res.data
     }
   }
--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import App from './App.vue'
 import router from './router'
 // 导入axios
 import axios from 'axios'
+// 导入element UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+// 添加element UI插件
+Vue.use(ElementUI)
 
 // 全局配置 axios
 // axios.defaults.baseURL = 'https://www.escook.cn'
