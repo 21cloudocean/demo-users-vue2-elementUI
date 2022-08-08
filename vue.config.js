63af19228ff9e3f18c1a82f31072b19c4f5c3ccc
21cloudocean <437508630@qq.com>
2022-08-08 15:03:47 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  devServer: {
+    // 修改 dev 期间的端口号
+    port: 3000,
+    // 自动打开浏览器
+    open: true
+  }
+}
