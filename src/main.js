654d1b7f99c09c57ea3c3fd8e6f28db90f404b91
21cloudocean <437508630@qq.com>
2022-08-08 15:16:37 +0800

    安装并配置axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 
 // 导入路由模块
 import router from './router'
+// 导入axios
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// 全局配置 axios
+axios.defaults.baseURL = 'https://www.escook.cn'
+Vue.prototype.$http = axios
 
 new Vue({
   // 挂载路由
